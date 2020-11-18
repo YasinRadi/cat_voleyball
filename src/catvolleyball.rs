2d2eea9cf26a533543b6facc37a811ad6c8e0714
Yasin Radi <yasin.ben.hamman@gmail.com>
2020-11-18 18:59:06 +0100

    Added texture and spritesheet loading.

--- src/catvolleyball.rs
+++ src/catvolleyball.rs
@@ -1,8 +1,16 @@
 use amethyst::{
     prelude::*,
     core::Transform,
-    renderer::Camera,
-    ecs::{Component, DenseVecStorage}
+    ecs::{Component, DenseVecStorage},
+    assets::{AssetStorage, Handle, Loader},
+    renderer::{
+        Camera,
+        Texture,
+        ImageFormat,
+        SpriteSheet,
+        SpriteRender,
+        SpriteSheetFormat
+    }
 };
 
 pub const ARENA_HEIGHT: f32 = 500.0;
@@ -10,6 +18,27 @@ pub const ARENA_WIDTH: f32 = 500.0;
 pub const PLAYER_WIDTH: f32 = 32.0;
 pub const PLAYER_HEIGHT: f32 = 22.0;
 
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/spritesheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/spritesheet.ron",
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store
+    )
+}
 
 fn initialize_camera(world: &mut World) {
     let mut transform = Transform::default();
@@ -25,23 +54,33 @@ fn initialize_camera(world: &mut World) {
         .build();
 }
 
-fn initialize_players(world: &mut World) {
+fn initialize_players(world: &mut World, sprite_sheet: Handle<SpriteSheet>) {
     let mut left_transform = Transform::default();
     let mut right_transform = Transform::default();
     let y = PLAYER_HEIGHT / 2.0;
 
+    // Set up both sides transforms
     left_transform.set_translation_xyz(PLAYER_WIDTH * 0.5, y, 0.0);
     right_transform.set_translation_xyz(
             ARENA_WIDTH - PLAYER_WIDTH * 0.5, 
             y, 0.0
     );
 
+    // Sprite render
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet.clone(),
+        // Cat will be the first sprite in list
+        sprite_number: 0
+    };
+
     world.create_entity()
+        .with(sprite_render.clone())
         .with(Player::new(Side::Left))
         .with(left_transform)
         .build();
 
     world.create_entity()
+        .with(sprite_render.clone())
         .with(Player::new(Side::Right))
         .with(right_transform)
         .build();
@@ -81,9 +120,10 @@ pub struct CatVolleyball;
 impl SimpleState for CatVolleyball {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
+        let sprite_sheet_handle = load_sprite_sheet(world);
 
         initialize_camera(world);
         world.register::<Player>();
-        initialize_players(world);
+        initialize_players(world, sprite_sheet_handle);
     }
 }
